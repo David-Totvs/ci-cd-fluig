ab13e9fff74773ce38500ee39654d214fb6e92c4
David-Totvs <david.allan@totvs.com.br>
2026-03-02 20:28:25 -0300

    teste01

---
+++ datasets/ds_ci_cd_sample.js
@@ -0,0 +1,15 @@
+function defineStructure() {
+
+}
+function onSync(lastSyncDate) {
+
+}
+function createDataset(fields, constraints, sortFields) {
+	var dataset = DatasetBuilder.newDataset();
+	dataset.addColumn('VERSION');
+	dataset.addRow(["0.1"]);
+	return dataset;
+}
+function onMobileSync(user) {
+
+}
